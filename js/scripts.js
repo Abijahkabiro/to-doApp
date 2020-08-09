3888cfb9f9f89dc364e6d315b9e9e8a9077263de
Abijah Kabiro <abijahkabiro@gmail.com>
2020-08-09 16:59:55 +0300

    create const declaration for CHECK,UNCHECK ,LINE_THROUGH and EDIT

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,9 @@ const todo= document.getElementById("todo");
 
 const options = {weekday:"long" , day:"numeric" , month:"long" , year:"numeric"}
 const today = new Date();
-date.innerHTML = today.toLocaleDateString("en-US" ,options);
+date.innerHTML = today.toLocaleDateString("en-US" ,options);//English (United States)
 
+const CHECK = "fa-check-square";
+const UNCHECK = "fa-square-o";
+const LINE_THROUGH ="lineThrough";
+const EDIT ="fa-pencil-square-o";
